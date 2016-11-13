4b15b74d3c19a87c3748757504b4bc0c97240d97
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2016-11-13 15:23:41 -0800

    editing and deleting a location

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -6,8 +6,14 @@ function newBeacon(){
         $( ".al-msg" ).effect( "bounce", { times: 3 }, "fast" );
         return;
     }
-    $(".al-msg").html("Click the map to choose the location of the new <b>Beacon</b>!");
+    $(".al-msg").html("Click the map to choose the location of the new <strong>Beacon</strong>! <p style='float:right;'><small>Click to cancel</small></p>");
     $(".al-wrap").fadeIn();
+    $(".al-wrap").click(function(){
+        if(capturePoints.call){
+            delete capturePoints.call;
+            $(".al-wrap").fadeOut();
+        }
+    });
     capturePoints.call=function(coords){
         showLoader();
         $(".al-wrap").fadeOut();
@@ -22,8 +28,14 @@ function newRFID(){
         $( ".al-msg" ).effect( "bounce", { times: 3 }, "fast" );
         return;
     }
-    $(".al-msg").html("Click the map to choose the location of the new <b>RFID Reader</b>.");
+    $(".al-msg").html("Click the map to choose the location of the new <strong>RFID Reader</strong>. <p style='float:right;'><small>Click to cancel</small></p>");
     $(".al-wrap").fadeIn();
+    $(".al-wrap").click(function(){
+        if(capturePoints.call){
+            delete capturePoints.call;
+            $(".al-wrap").fadeOut();
+        }
+    });
     capturePoints.call=function(coords){
         showLoader();
         $(".al-wrap").fadeOut();
@@ -37,9 +49,39 @@ function newLocationExec(type){
     var data = keyValue({label:".inputLabel", name:".inputName", reference:".inputReference", lat:".inputLat", lng:".inputLng"});
     data['type']=type;
     call('new_location',data,function(r){
-
+        socket.emit('index');
+        showLoader();
     });
 }
 var capturePoints={
     call:null
 };
+function openLocation(name){
+    socket.emit('edit_location', {name:name});
+    showLoader();
+}
+function deleteLocation(name){
+    var values = keyValue({name_key:".inputNameKey"});
+    call('delete_location', values, function(data){
+        if(data.code=="1"){
+            socket.emit('index');
+            showLoader();
+        }else{
+            $('.edit-box').effect('shake');
+        }
+    });
+}
+function saveLocation(){
+    var values = keyValue({label:".inputLabel", name:".inputName", reference:".inputReference", lat:".inputLat", lng:".inputLng", name_key:".inputNameKey"});
+    call('save_location', values, function(data){
+        if(data.code=="1"){
+            socket.emit('index');
+            showLoader();
+        }else{
+            $('.edit-box').effect('shake');
+        }
+    });
+}
+function openLocationEvents(){
+    
+}
