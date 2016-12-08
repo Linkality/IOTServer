c4379d1f8951682c6cefcf1f007f67a03b607e48
nathanieldavidson <ndavidson@csumb.edu>
2016-12-07 18:06:04 -0800

    added room generation, added app connection beginning

--- bin/server.js
+++ bin/server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 var port = parseInt(process.env.PORT) | 8080;
-
+var util = require('util');
 var app = require('../app');
 app.set('port', port);
 var http = require('http');
@@ -19,7 +19,11 @@ if(process.env.mdb_user==null){
     process.env.mdb_host="olympia.modulusmongo.net:27017";
     process.env.mdb_db="xuDihy4d";
 }
+var eventRegistration = {
+    onLocation:{
 
+    }
+};
 var url = 'mongodb://'+process.env.mdb_user+':'+process.env.mdb_pass+'@'+process.env.mdb_host+'/'+process.env.mdb_db;
 var mdb;
 MongoClient.connect(url, function(err, db) {
@@ -28,13 +32,62 @@ MongoClient.connect(url, function(err, db) {
 var init = new Loader();
 
 init.addSetting("title", "Linkality");
-
 io.on('connection', function (socket) {
     init.generateSession(socket);
     init.exec(socket, mdb);
     socket.on('disconnect', function (c) {
         init.deleteSession(socket);
     });
+});
+var WebSocketClient = require('websocket').client;
+
+var client = new WebSocketClient();
+
+var buffer = new Buffer(util.format("%s:%s", 'nathaniel.davidson@gmail.com/progressfollowers', 'Nat15678!'));
+
+var auth = util.format("Basic %s", buffer.toString('base64'));
+var uri = util.format("wss://dap.amtech.mx/amtech/push/notifications");
+
+// amtech websocket Initializization.
+client.connect(uri, null, null, {
+    "Authorization" : auth
+});
+var History = require("../lib/models/History.js");
+var history = new History();
+var Location = require("../lib/models/Location.js");
+var location = new Location();
+client.on('connect', function(connection) {
+    _connection = connection
+
+    var url = util.format('http://%s:%s/api/track','localhost', server.address().port);
+    connection.on('error', function(error) {
+        console.log("Connection Error: " + error.toString());
+    });
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            var data = message.utf8Data;
+            var json = JSON.parse(data);
+            var antenna = json.body.match(/From (.*?) -/i)[1];
+            var tagId = json.body.match(/epcString: (.*?) -/i)[1];
+            location.getByName(mdb, antenna, function(loc){
+                if(loc) {
+                    console.log(loc);
+                    history.new(mdb, loc['_id'], tagId, function (d) {
+                        console.log(d);
+                    });
+                }else{
+                    console.log("Antenna not found! "+antenna);
+                }
+            });
+        }
+    });
+
+    connection.on('close', function() {
+        console.log('echo-protocol Connection Closed');
+    });
+});
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
 });
 
 
