5ddbca3475680a6ab67131542c78c52dc7af9086
nathanieldavidson <ndavidson@csumb.edu>
2016-12-15 20:59:27 -0800

    friend finder etc

--- bin/server.js
+++ bin/server.js
@@ -8,6 +8,8 @@ var util = require('util');
 var app = require('../app');
 app.set('port', port);
 var http = require('http');
+var SimpleName = require('../lib/models/SimpleName.js');
+var simple = new SimpleName();
 var MongoClient = require('mongodb').MongoClient;
 var server = http.createServer(app);
 var io = require('socket.io')(server);
@@ -33,11 +35,15 @@ MongoClient.connect(url, function(err, db) {
 var init = new Loader();
 
 init.addSetting("title", "Linkality");
+var connected = new Array();
 io.on('connection', function (socket) {
     init.generateSession(socket);
     init.exec(socket, mdb);
+    connected.push(socket);
     socket.on('disconnect', function (c) {
         init.deleteSession(socket);
+        connected.splice(connected.indexOf(socket), 1);
+        console.log("in arr: "+connected.length);
     });
 });
 var WebSocketClient = require('websocket').client;
@@ -47,7 +53,7 @@ var client = new WebSocketClient();
 var buffer = new Buffer(util.format("%s:%s", 'nathaniel.davidson@gmail.com/progressfollowers', 'Nat15678!'));
 
 var auth = util.format("Basic %s", buffer.toString('base64'));
-var uri = util.format("wss://dap.amtech.mx/amtech/push/notifications");
+var uri = util.format("wss://dap.amtech.mx/amtech/push/notifications?clientid=nd03");
 
 // amtech websocket Initializization.
 client.connect(uri, null, null, {
@@ -63,12 +69,27 @@ client.on('connect', function(connection) {
         console.log("Connection Error: " + error.toString());
     });
     connection.on('message', function(message) {
-        console.log(message);
+        //console.log(message);
         if (message.type === 'utf8') {
             var data = message.utf8Data;
             var json = JSON.parse(data);
             var antenna = json.body.match(/From (.*?) -/i)[1];
             var tagId = json.body.match(/epcString: (.*?) -/i)[1];
+            if(antenna=="ProgressBridgeConfigDemo:b8:27:eb:bd:89:ee:llrpAntenna1ConfigExample-Sargas"){
+                if(mdb!=null) {
+                    simple.getByReference(mdb, tagId, function(simpleTag){
+                        if(simpleTag==null){
+                            for(var i=0;i<connected.length;i++) {
+                                connected[i].emit("foundNonSimpleTag", {tagId: tagId});
+                            }
+                        }else{
+                            for(var i=0;i<connected.length;i++) {
+                                connected[i].emit("foundSimpleTag", simpleTag);
+                            }
+                        }
+                    });
+                }
+            }
             location.getByReference(mdb, antenna, function(loc){
                 if(loc) {
                     console.log(loc);
--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -33,3 +33,27 @@ function keyValue(data){
     });
     return obj;
 }
+function addTags(){
+    socket.emit('generate_secret');
+}
+function generateKey(func){
+    call('generate_secret', {generate:true}, function(data){
+        $("#inputSecret").val(data.string);
+        $("#ffe").css({"backgroundColor":"#fff"});
+        func();
+    });
+}
+function readTag(){
+    generateKey(function(){
+        saveSecret();
+    });
+}
+function saveSecret(){
+    call('generate_secret', keyValue({reference:"#inputReference", name:"#inputSecret"}), function(data){
+        if(data.code==0){
+            $("#ffe").css({"backgroundColor":"#f00"});
+        }else{
+            $("#ffe").css({"backgroundColor":"#0f0"});
+        }
+    });
+}
